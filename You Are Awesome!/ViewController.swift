b49b03db969521e6e171dbe9275ea2f745e8edc5
Larry Goodman <wetutorthat@gmail.com>
2021-06-25 13:32:52 -0400

    Added a messageLabel and show pressed button. Added @IBOutlet to messageLabel and add @IBAction to Show Message button.

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       print("👹 viewDidLoad has run")
+        messageLabel.text = "Fabulous? That's You!"
     }
 
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        print("👽 The message button was pressed!")
+        messageLabel.text = "You Are Awesome!"
+    }
+    
 }
 
